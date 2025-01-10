61de52cf6cbab9489293b99fcb3f748df1d5596a
SureshBritt <sureshornirmal@gmail.com>
2025-01-09 23:10:01 -0600

    Abstractclass/creating and writing txt file/File reader /try with resources

---
+++ src/AbstractClassExample.java
@@ -0,0 +1,15 @@
+public class AbstractClassExample {
+
+    public static void main(String[] args) {
+        CameraforAbstractClassExample cam1 = new CameraforAbstractClassExample();
+        cam1.setId(5);
+
+        CarForAbstractClassExample car1 = new CarForAbstractClassExample();
+        car1.setId(4);
+
+        car1.run();
+
+        //Machine machine1 = new Machine();
+    }
+
+}
